c71b3f1ada34d0a73899694222988d2c668dbf91
Randy <randy@clarkehill.com>
2021-04-07 19:12:54 -0700

    Initial Commit

---
+++ Shared/ArbitrageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArbitrageApp.swift
+//  Shared
+//
+//  Created by Randy Hill on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ArbitrageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
